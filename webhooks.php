a8c40aa4149fb7cc945a62c4364e9408ee620607
jakudon <63390633+jakudon@users.noreply.github.com>
2020-04-09 14:05:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'bfn/LXDNdIvfbEubfSVBJ21bnn/zX1lrdcb0mweiJupn85cmutq/518/dyvvPI2Y6j5TkD/1z1B7eVdFdAHqDhKm2GF8uVRiuZPwwYeK/DCbZDwav4xoRPZFtc0Epaa7LaYZnMQu+R6OZ3U/7lPFTwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
